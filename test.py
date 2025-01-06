4527746b1938adcdccb73cbb3a178dd4d1643357
sally957 <3d9299@gmail.com>
2025-01-06 13:09:27 +0800

    123

---
+++ test.py
@@ -0,0 +1,7 @@
+import       // from doOS mtdule for interacting with the operating eystem.       nv import load_dotenv;     // use 'pip install python-dotenv' to add this library if not already installed                 
+load_dotenv(dotenv_path="./.en;     // use 'pip install python-dotenv' to add this library if not already installed                 v")  // loads variables from '.env' into environment                                         
+data = '{ "cdotenv_path=on/.ten{ /e// lnade variables from 'rtel' rntosepvironmennsprin                                    
+imto(e nequests   //sequests,is)a Py/hon/mpdrle usentfor mak sgeHTTP rvquests'                                    nd in console                           
+data='{"contents":[{ "parts": [{"t": "ExpihowAIworks"}]}]}';
+=''+os.getenv(")   // Get the value of GEMINI API KEY from .env file andaddittoURLheaders={''''}response=requests.post(url,headers=headersdata=data)#senPOSTrequestwithabvamers             
+it'Respse:'spns.;// sevr' repon in consle                           
